8495b98c75e68ed1fd3545e36a176d292ca9972b
AnolKurian-gep <70510416+AnolKurian-gep@users.noreply.github.com>
2020-11-01 11:34:41 +0530

    update

--- app.py
+++ app.py
@@ -70,6 +70,23 @@ class Buyers_id(Resource):
         result = str(db.buyer.find_one({'buyer_id':buyer_id},{'_id':0}))
         return jsonify(result) 
 
+class Orders(Resource):
+    def get(self):
+        result= ""
+        result1= ""
+        array = []
+        data = db.previous_orders.find({}, {'_id': 0})   
+        for x in data:
+            result = result +","+  str(x)
+        result = result[1:]
+        return jsonify(result)
+
+class Orders_id(Resource):
+    def get(self,orders_id):
+        print (orders_id)
+        result= ""
+        result = str(db.previous_orders.find_one({'order_no':orders_id},{'_id':0}))
+        return jsonify(result) 
 
 api.add_resource(Products, '/products')
 api.add_resource(Categories, '/products/<product_id>') # Route_3
@@ -77,6 +94,8 @@ api.add_resource(Suppliers, '/suppliers')
 api.add_resource(Suppliers_id, '/suppliers/<supplier_id>')
 api.add_resource(Buyers, '/buyers')
 api.add_resource(Buyers_id, '/buyers/<buyer_id>')
+api.add_resource(Orders, '/orders')
+api.add_resource(Orders_id, '/orders/<orders_id>')
 
 if __name__ == '__main__':
    app.run(port=5002)
